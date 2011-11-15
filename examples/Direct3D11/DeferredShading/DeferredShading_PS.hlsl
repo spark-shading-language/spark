a04d237dc222d1c6fd3b4b9390a2d9d6e65b3997
Rahul Sathe <rahul.p.sathe@intel.com>
2011-11-15 14:44:41 -0800

    Added a G-buffer light pass.
    Signed-off-by: Tim Foley <tim.foley.is@gmail.com>

--- examples/Direct3D11/DeferredShading/DeferredShading_PS.hlsl
+++ examples/Direct3D11/DeferredShading/DeferredShading_PS.hlsl
@@ -16,8 +16,9 @@ cbuffer cbPerObject : register( b0 )
 
 cbuffer cbPerFrame : register( b1 )
 {
-	float3		g_vLightDir				: packoffset( c0 );
-	float		g_fAmbient				: packoffset( c0.w );
+    matrix      mCameraProj             : packoffset( c0);
+	float3		g_vLightDir				: packoffset( c4 );
+	float		g_fAmbient				: packoffset( c4.w );
 };
 
 //--------------------------------------------------------------------------------------
@@ -84,21 +85,21 @@ float4 PSMain( PS_INPUT Input ) : SV_TARGET
 	fLighting = max( fLighting, g_fAmbient );
 	return vDiffuse * fLighting;
 
-    float3 vView = normalize(Input.vViewVector);
-    float3 vNormal = normalize(Input.vNormal);
-    float3 vLightDir = normalize(g_vLightDir);
-
-    //  Specular lighting.
-    float fNormalDotLight = saturate ( dot(vNormal, vLightDir) );  
-    float3 vLightReflect = 2.0 * fNormalDotLight * vNormal - vLightDir;
-    float fViewDotReflect = saturate( dot(vView, vLightReflect) );
-    float fSpecIntensity1 = pow(fViewDotReflect, 2.0f);
-    float4 fSpecIntensity = (float4)fSpecIntensity1;
-
-    float4 colSpecular = float4(1.0f, 1.0f, 1.0f, 1.0f);
-    float4 lightSpecular = 0.1 * float4(1.0f, 1.0f, 1.0f, 1.0f);
-    float4 specular = fSpecIntensity * colSpecular * lightSpecular;
-	return specular;
+//    float3 vView = normalize(Input.vViewVector);
+//    float3 vNormal = normalize(Input.vNormal);
+//    float3 vLightDir = normalize(g_vLightDir);
+//
+//    //  Specular lighting.
+//    float fNormalDotLight = saturate ( dot(vNormal, vLightDir) );  
+//    float3 vLightReflect = 2.0 * fNormalDotLight * vNormal - vLightDir;
+//    float fViewDotReflect = saturate( dot(vView, vLightReflect) );
+//    float fSpecIntensity1 = pow(fViewDotReflect, 2.0f);
+//    float4 fSpecIntensity = (float4)fSpecIntensity1;
+//
+//    float4 colSpecular = float4(1.0f, 1.0f, 1.0f, 1.0f);
+//    float4 lightSpecular = 0.1 * float4(1.0f, 1.0f, 1.0f, 1.0f);
+//    float4 specular = fSpecIntensity * colSpecular * lightSpecular;
+//	return specular;
 }
 
 //--------------------------------------------------------------------------------------
@@ -109,6 +110,10 @@ struct GBuffer
     float4 albedo : SV_Target1;
     float2 positionZGrad : SV_Target2;
 };
+
+// Above values PLUS depth buffer (last element)
+Texture2DMS<float4, 1> gGBufferTextures[4] : register(t0);
+
 
 float2 EncodeSphereMap(float3 n)
 {
@@ -139,5 +144,118 @@ void GBufferPS(PS_INPUT input, out GBuffer outputGBuffer)
     outputGBuffer.albedo = surface.albedo;
     outputGBuffer.positionZGrad = float2(ddx_coarse(surface.positionView.z),
                                          ddy_coarse(surface.positionView.z));
+}
+
+
+// Full screen pass
+
+struct FullScreenTriangleVSOut
+{
+    float4 positionViewport : SV_Position;
+};
+
+FullScreenTriangleVSOut FullScreenTriangleVS(uint vertexID : SV_VertexID)
+{
+    FullScreenTriangleVSOut output;
+
+    // Parametrically work out vertex location for full screen triangle
+    float2 grid = float2((vertexID << 1) & 2, vertexID & 2);
+    output.positionViewport = float4(grid * float2(2.0f, -2.0f) + float2(-1.0f, 1.0f), 0.0f, 1.0f);
+    
+    return output;
+}
+
+
+//--------------------------------------------------------------------------------------
+float3 ComputePositionViewFromZ(float2 positionScreen,
+                                float viewSpaceZ)
+{
+    float2 screenSpaceRay = float2(positionScreen.x / mCameraProj._11,
+                                   positionScreen.y / mCameraProj._22);
+    
+    float3 positionView;
+    positionView.z = viewSpaceZ;
+    // Solve the two projection equations
+    positionView.xy = screenSpaceRay.xy * positionView.z;
+    
+    return positionView;
+}
+SurfaceData ComputeSurfaceDataFromGBufferSample(uint2 positionViewport, uint sampleIndex)
+{
+    // Load the raw data from the GBuffer
+    GBuffer rawData;
+    rawData.normal_specular = gGBufferTextures[0].Load(positionViewport.xy, sampleIndex).xyzw;
+    rawData.albedo = gGBufferTextures[1].Load(positionViewport.xy, sampleIndex).xyzw;
+    rawData.positionZGrad = gGBufferTextures[2].Load(positionViewport.xy, sampleIndex).xy;
+    float zBuffer = gGBufferTextures[3].Load(positionViewport.xy, sampleIndex).x;
+    
+    float2 gbufferDim;
+    uint dummy;
+    gGBufferTextures[0].GetDimensions(gbufferDim.x, gbufferDim.y, dummy);
+    
+    // Compute screen/clip-space position and neighbour positions
+    // NOTE: Mind DX11 viewport transform and pixel center!
+    // NOTE: This offset can actually be precomputed on the CPU but it's actually slower to read it from
+    // a constant buffer than to just recompute it.
+    float2 screenPixelOffset = float2(2.0f, -2.0f) / gbufferDim;
+    float2 positionScreen = (float2(positionViewport.xy) + 0.5f) * screenPixelOffset.xy + float2(-1.0f, 1.0f);
+    float2 positionScreenX = positionScreen + float2(screenPixelOffset.x, 0.0f);
+    float2 positionScreenY = positionScreen + float2(0.0f, screenPixelOffset.y);
+        
+    // Decode into reasonable outputs
+    SurfaceData data;
+        
+    // Unproject depth buffer Z value into view space
+    float viewSpaceZ = mCameraProj._43 / (zBuffer - mCameraProj._33);
+
+    data.positionView = ComputePositionViewFromZ(positionScreen, viewSpaceZ);
+//    data.positionViewDX = ComputePositionViewFromZ(positionScreenX, viewSpaceZ + rawData.positionZGrad.x) - data.positionView;
+//    data.positionViewDY = ComputePositionViewFromZ(positionScreenY, viewSpaceZ + rawData.positionZGrad.y) - data.positionView;
+
+    data.normal = DecodeSphereMap(rawData.normal_specular.xy);
+    data.albedo = rawData.albedo;
+
+    data.specularAmount = rawData.normal_specular.z;
+    data.specularPower = rawData.normal_specular.w;
+    
+    return data;
+}
+
+float4 BasicLoop(FullScreenTriangleVSOut input, uint sampleIndex)
+{
+    // How many total lights?
+//    uint totalLights, dummy;
+//    gLight.GetDimensions(totalLights, dummy);
+//    
+//    float3 lit = float3(0.0f, 0.0f, 0.0f);
+//
+//    [branch] if (mUI.visualizeLightCount) {
+//        lit = (float(totalLights) * rcp(255.0f)).xxx;
+//    } else {
+
+        SurfaceData surface = ComputeSurfaceDataFromGBufferSample(uint2(input.positionViewport.xy), sampleIndex);
+        float4 vDiffuse = surface.albedo;
+        
+        float fLighting = saturate( dot( g_vLightDir, surface.normal) );
+        fLighting = max( fLighting, g_fAmbient );
+        return vDiffuse * fLighting;
+        return float4(surface.normal, 1.0f);
+
+//        // Avoid shading skybox/background pixels
+//        if (surface.positionView.z < mCameraNearFar.y) {
+//            for (uint lightIndex = 0; lightIndex < totalLights; ++lightIndex) {
+//                PointLight light = gLight[lightIndex];
+//                AccumulateBRDF(surface, light, lit);
+//            }
+//        }
+//    }
+     
+//    return float4(lit, 1.0f);
+}
+
+float4 LightPS(FullScreenTriangleVSOut input) : SV_Target
+{
+    // Shade only sample 0
+    return BasicLoop(input, 0);
 }
 
